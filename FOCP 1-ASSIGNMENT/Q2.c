89926dcd5634136ccbaf829256b3778a706b57af
sonakshi25csu234-cloud <sonakshi25csu234@ncuindia.edu>
2025-11-02 13:59:09 +0530

    HCF calculation program in Q2.c

---
+++ FOCP 1-ASSIGNMENT/Q2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int hcf(int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return hcf(b, a % b);
+}
+
+int main() {
+    int num1, num2;
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("HCF of %d and %d is %d\n", num1, num2, hcf(num1, num2));
+
+    return 0;
+}
