d931b015556dc58c758e58b25cd3d16e3ea8c7a6
sonakshi25csu234-cloud <sonakshi25csu234@ncuindia.edu>
2025-11-02 23:17:59 +0530

    Add prime number counting program in Q14.c

---
+++ FOCP 1-ASSIGNMENT/Q14.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1)
+        return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int n, count = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (isPrime(arr[i]))
+            count++;
+    }
+
+    printf("\nTotal number of prime numbers in the array: %d\n", count);
+
+    return 0;
+}
